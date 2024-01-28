268cdcf4dd97390dcf722ee4e5babd20edeaa0e9
bruteF04C3 <rishabhonweb@gmail.com>
2024-01-28 21:23:13 +0530

    Add initial components

---
+++ src/app/components/icon-card/icon-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+@Component({
+  selector: 'app-icon-card',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './icon-card.component.html',
+  styleUrl: './icon-card.component.sass'
+})
+export class IconCardComponent {
+
+}
---
+++ src/app/components/icons-list/icons-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Icon } from '../../icon.interface';
+
+@Component({
+  selector: 'app-icons-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './icons-list.component.html',
+  styleUrl: './icons-list.component.sass',
+})
+export class IconsListComponent {
+  
+}
