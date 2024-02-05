9e1376311974f9bfbf7cf8110ba1e09674121211
bruteF04C3 <rishabhonweb@gmail.com>
2024-02-05 18:15:19 +0530

    Add search UI

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [FormsModule, MatFormField, MatInput, MatIcon],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+export class SearchComponent {
+  searchQuery: string = '';
+}
