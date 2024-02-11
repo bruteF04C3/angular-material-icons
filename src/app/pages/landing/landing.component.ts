6817d83bb13f25e1f776bf1ad38b60e7bbb7e0fb
bruteF04C3 <rishabhonweb@gmail.com>
2024-02-11 09:47:25 +0530

    Add link to icons page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { WELCOME_MESSAGE } from '../../constants';
+import { SHOW_ENTIRE_LIST, WELCOME_MESSAGE } from '../../constants';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
   welcomeMessage: string = WELCOME_MESSAGE;
+  entireListMessage: string = SHOW_ENTIRE_LIST;
 }
